2370ff91c91d393865f576709a4716205a02451c
qihang <im.qihang@gmail.com>
2023-08-14 20:34:14 +0800

    Add DateUtil.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # my-personal-utils
+
+## DateUtil
+
+### static methods
+
+| name | type | description |
+| ---- | ---- | ----------- |
+| format | `(date: number | string | Date, format: string) => string` | format Date to string |
+
+### class methods
+
+| name | type | description |
+| ---- | ---- | ----------- |
+| constructor | `(date: number | string | Date) => DateUtil` | create a instance of DateUtil |
+| getDate | `() => Date` | get Date instance of current DateUtil |
+| setDate | `(date: number | string | Date) => DateUtil` | set Date instance of current DateUtil |
+| format | `(format?: string) => string` | same as the static method |
---
+++ src/DateUtil.js
@@ -0,0 +1,86 @@
+class DateUtil {
+    /**
+     * Format date to string
+     * @param date Date you want format
+     * @param format Format rule
+     * @returns String
+     */
+    static format(date, format = '') {
+        let _date = null;
+        if (date instanceof Date) {
+            _date = date;
+        }
+        else {
+            _date = new Date(date);
+        }
+        let formatObj = {
+            'M+': _date.getMonth() + 1,
+            'd+': _date.getDate(),
+            'H+': _date.getHours(),
+            'm+': _date.getMinutes(),
+            's+': _date.getSeconds(),
+            'q+': Math.floor((_date.getMonth() + 3) / 3),
+            'S': _date.getMilliseconds()
+        };
+        // replace year
+        if (/(y+)/.test(format)) {
+            format = format.replace(RegExp.$1, `${_date.getFullYear()}`.substr(4 - RegExp.$1.length));
+        }
+        // replace month day hour minute second
+        Object.keys(formatObj)
+            .forEach((k) => {
+            if (new RegExp(`(${k})`).test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length === 1
+                    ? ('' + formatObj[k])
+                    : `00${formatObj[k]}`.substr(`${formatObj[k]}`.length));
+            }
+        });
+        return format;
+    }
+    /**
+     * constructor
+     * @param p Date string or timestamp or Date object
+     */
+    constructor(p) {
+        this.date = null;
+        this.formatString = 'yyyy-MM-dd HH:mm:ss';
+        this.setDate(p);
+    }
+    /**
+     * Get current date object
+     * @returns Date object
+     */
+    getDate() {
+        return this.date;
+    }
+    /**
+     * Set date object
+     * @param date Date string or timestamp or Date object
+     */
+    setDate(date) {
+        if (date instanceof Date) {
+            this.date = date;
+        }
+        else if (date) {
+            this.date = new Date(date);
+        }
+        else {
+            this.date = new Date();
+        }
+        return this;
+    }
+    /**
+     * Format date to string
+     * @param string Format rule, if null, it will be yyyy-MM-dd HH:mm:ss
+     * @returns String
+     */
+    format(string = null) {
+        if (!this.date)
+            return '';
+        if (string === null) {
+            return DateUtil.format(this.date, this.formatString);
+        }
+        return DateUtil.format(this.date, string);
+    }
+}
+export default DateUtil;
